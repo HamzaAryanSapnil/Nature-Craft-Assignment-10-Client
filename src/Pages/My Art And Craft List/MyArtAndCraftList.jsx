d6bf2a0d50b6266907dd8147c3d8ad92aa25e764
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2024-04-29 00:11:23 +0600

    got the data from server

--- src/Pages/My Art And Craft List/MyArtAndCraftList.jsx
+++ src/Pages/My Art And Craft List/MyArtAndCraftList.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
+import useAuth from "../../Hooks/Use Auth Context/UseAuthContext";
 
 const MyArtAndCraftList = () => {
+    const user = useAuth();
+    const [items, setItems] = useState([]);
+    const email = user?.user?.email;
+    useEffect(() => {
+      fetch(`http://localhost:3000/myArtAndCraftList/${email}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setItems(data);
+      })
+    
+      
+    }, )
+    
     return (
         <div>
-            My Art And Craft List
+            My Art And Craft List: {items.length}
         </div>
     );
 };
