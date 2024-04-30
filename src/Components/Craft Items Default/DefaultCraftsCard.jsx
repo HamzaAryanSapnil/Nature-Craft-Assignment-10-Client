396b159d26e4a4243aeea66b448a2468629e224e
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2024-04-30 15:31:31 +0600

    added craft section

---
+++ src/Components/Craft Items Default/DefaultCraftsCard.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom';
+const DefaultCraftsCard = ({defaultCraft}) => {
+    const {photoUrl, item_name, sub_category} = defaultCraft;
+    console.log(defaultCraft, photoUrl);
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src={defaultCraft?.photoUrl ? defaultCraft?.photoUrl :"https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"}
+          alt="Shoes"
+          className="h-72"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{item_name}</h2>
+        <p>{defaultCraft?.short_des}</p>
+        <div className="card-actions justify-end">
+          <Link to={`/craftDetails/${sub_category}`} ><button className="btn btn-primary">View Details</button></Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+DefaultCraftsCard.propTypes = {
+    defaultCraft: PropTypes.object,
+}
+export default DefaultCraftsCard;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,17 +1,26 @@
 import Banner from "../../Components/Banner/Banner";
+import CraftItemsDefault from "../../Components/Craft Items Default/CraftItemsDefault";
 import SubCategoryCards from "../../Components/Sub Category/SubCategoryCards";
 
 const Home = () => {
   return (
-    <div className="" >
+    <div className="">
       {/* banner */}
       <div className="h-72 md:h-[100vh] overflow-hidden">
         <Banner></Banner>
       </div>
 
+      {/* default craft items */}
+      <div className="my-10">
+        <header className="my-10 text-center" >
+          <h1>Craft Items</h1>
+          <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic quod repellat dicta, illo animi magnam. Voluptas praesentium delectus illo enim quisquam saepe aliquam natus possimus sed nemo consequuntur, iusto architecto.</h1>
+        </header>
+        <CraftItemsDefault></CraftItemsDefault>
+      </div>
 
       {/* sub category */}
-      <div className="h-[400px] container mx-auto my-10" >
+      <div className="h-[400px] container mx-auto my-10">
         <SubCategoryCards></SubCategoryCards>
       </div>
     </div>
