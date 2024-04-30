4d45e6e3791dac95347a2af33386e7c1fa9cc1bc
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2024-04-30 17:39:53 +0600

    all art and craft done

--- src/Pages/All Art and Craft Items/AllArtAndCraftItems.jsx
+++ src/Pages/All Art and Craft Items/AllArtAndCraftItems.jsx
@@ -1,22 +1,27 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 const AllArtAndCraftItems = () => {
   const allCrafts = useLoaderData();
   return (
     <div className="container grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mx-auto justify-items-center justify-center items-center p-4 gap-10 " >
       {allCrafts.map((craft) => (
+        
         <div className="card w-96 bg-base-100 shadow-xl" key={craft._id}>
           <figure>
             <img
-              src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+              src={craft?.photoUrl? craft?.photoUrl : "https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"}
               alt="Shoes"
+              className="w-full h-64"
             />
           </figure>
           <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <h2 className="card-title">{craft.item_name}</h2>
+            <p>{craft.sub_category}</p>
+            <p>{craft.short_des}</p>
+            <p>Price: {craft.price}</p>
+            <p>Rating: {craft.rating}</p>
             <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
+              <Link to={`/craftDetails/${craft._id}`} ><button className="btn btn-primary">View Details</button></Link>
             </div>
           </div>
         </div>
