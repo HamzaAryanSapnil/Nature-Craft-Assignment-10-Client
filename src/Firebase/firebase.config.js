23162c6e130af687439be595422d3090261c3a04
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2024-04-30 09:08:12 +0600

    env done

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAzMW5a2GJqt9EXkjnlCs2Efh2y9DOIcFI",
-  authDomain: "assignment-10-art-and-craft.firebaseapp.com",
-  projectId: "assignment-10-art-and-craft",
-  storageBucket: "assignment-10-art-and-craft.appspot.com",
-  messagingSenderId: "345995672577",
-  appId: "1:345995672577:web:1bd802e604e4ef90849400"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
